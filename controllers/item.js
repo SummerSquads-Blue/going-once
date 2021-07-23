1ec7d8dbfa01a136eeb870fee815af2026f0f6a1
RadarRadarRadar <mharold214@gmail.com>
2021-07-23 13:34:02 -0500

    Rebase with development branch

--- controllers/item.js
+++ controllers/item.js
@@ -2,7 +2,6 @@ const Item = require('../models/item');
 
 module.exports = {
   index,
-  tagIndex,
   findItemById
 };
 
@@ -10,10 +9,11 @@ function index(req, res) {
   Item.find({}).then(items => res.json(items))
 }
 
-function tagIndex(req, res) {
-    Item.find({}).then(items => res.json(items))
-}
+// function tagIndex(req, res) {
+//     Item.find({ tag: req.tag }).then(items => res.json(items))
+// }
 
 function findItemById(req, res) {
     Item.findById(req.id).then(item => res.json(item))
 }
+
